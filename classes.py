1e65e1949fe52dd3f8def8f832eadaa26e19211b
agolovanov <alexandergolovanovvla@gmail.com>
2024-03-22 18:26:36 +0300

    add 13.2

--- classes.py
+++ classes.py
@@ -5,17 +5,41 @@ class Category:
     def __init__(self, name, description):
         self.name = name
         self.description = description
-        self.products = []
+        self.__products = []
         Category.total_categories += 1
 
     def add_product(self, product):
-        self.products.append(product)
+        self.__products.append(product)
         Category.total_unique_products.add(product.name)
+
+    @property
+    def products(self):
+        result = ""
+        for product in self.__products:
+            result += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return result
+
 
 
 class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def create_product(cls, name, description, price, quantity):
+        return cls(name, description, price, quantity)
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price <= 0:
+            print("Ошибка: введена некорректная цена.")
+        else:
+            self.__price = new_price
+
